37d35393d5257f880a6a9ab55502c7880fbd0c42
DanielaSilvanaTochi <133720661+DanielaS-Tochi@users.noreply.github.com>
2025-08-01 19:30:55 +0000

    Update setup files

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,23 +1,18 @@
 #!/usr/bin/env bash
 set -e
 
-# Intenta instalar desde get.sui.io
-if ! curl -fsSL https://get.sui.io | bash; then
-  echo "Fallo la instalación desde get.sui.io, intentando desde GitHub Releases..."
+# Versión de SUI a instalar
+SUI_VERSION="mainnet-v1.22.0"
+SUI_TAR="sui-${SUI_VERSION}-ubuntu-x86_64.tar.gz"
+SUI_URL="https://github.com/MystenLabs/sui/releases/download/${SUI_VERSION}/${SUI_TAR}"
 
-  # Define la versión que deseas instalar
-  SUI_VERSION="mainnet-v1.22.0"
-  SUI_TAR="sui-${SUI_VERSION}-ubuntu-x86_64.tar.gz"
-  SUI_URL="https://github.com/MystenLabs/sui/releases/download/${SUI_VERSION}/${SUI_TAR}"
+# Descarga y extrae el binario
+wget -q $SUI_URL
+tar -xzf $SUI_TAR
 
-  # Descarga y extrae
-  wget -q $SUI_URL
-  tar -xzf $SUI_TAR
-
-  # Mueve el binario a /usr/local/bin
-  sudo mv sui /usr/local/bin/
-  rm $SUI_TAR
-fi
+# Mueve el binario a /usr/local/bin (requiere permisos de superusuario)
+sudo mv sui /usr/local/bin/
+rm $SUI_TAR
 
 # Verifica la instalación
 sui --version
